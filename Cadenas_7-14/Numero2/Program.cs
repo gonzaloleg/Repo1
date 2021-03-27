602bd5961c6ae3db519b05946061354acd2ac217
gonzaloleg <70615605+gonzaloleg@users.noreply.github.com>
2021-03-27 17:12:57 -0300

    ..

---
+++ Cadenas_7-14/Numero2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numero2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase;
+            Console.WriteLine("Hola. Ingrese una frase:");
+            frase = Console.ReadLine();
+            if (frase.Contains("fin"))
+            {
+                Console.WriteLine("Su frase contiene la palabra 'fin'.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
