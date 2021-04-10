e3558e033e21706e48803a76c9e2ffb1a1434848
gonzaloleg <70615605+gonzaloleg@users.noreply.github.com>
2021-04-10 15:25:12 -0300

    .

---
+++ Fechas_21-30/Numero21/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numero21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime fecha = DateTime.Now;
+            Console.WriteLine($"Hoy es {fecha}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Fechas_21-30/Numero22/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numero22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime fecha = DateTime.Now;
+            Console.WriteLine($"Hoy es {fecha.ToString("dd/MM/yyyy")}");
+            Console.ReadKey();
+        }
+    }
+}
